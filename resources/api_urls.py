94b395586ec86bd538e0e8669465cd5547f8aa6b
gs-digital-solution <titus.asaph18@gmail.com>
2025-08-26 01:27:12 +0200

    modifications du 25 aout 2025 à 23h00

--- resources/api_urls.py
+++ resources/api_urls.py
@@ -1,7 +1,3 @@
-from django.urls import path
-from .api_views import PaysListAPIView, SousSystemeListAPIView
 
-urlpatterns = [
-    path('pays/', PaysListAPIView.as_view(), name='api_pays_list'),
-    path('sous-systeme/', SousSystemeListAPIView.as_view(), name='api_soussysteme_list'),
-]
+
+
