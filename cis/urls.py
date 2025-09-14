7cd9c370a195d913e9f32f8e7a2e839b93c3b619
gs-digital-solution <titus.asaph18@gmail.com>
2025-09-14 16:33:01 +0200

    ok

--- cis/urls.py
+++ cis/urls.py
@@ -18,54 +18,43 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path, include
-from django.contrib.auth.views import  LogoutView
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path(
-        'resources/',
-        include('resources.urls')
-    ),
-    path(
-        'ckeditor/',
-        include('ckeditor_uploader.urls')),
-path(
-    'correction/',
-    include('correction.urls')),
 
-path(
-    'logout/',
-    LogoutView.as_view(next_page='correction:soumettre'),
-    name='logout'),
+    # ... vos routes “resources/”, “ckeditor/”, “correction/”, etc. ...
+    path('resources/', include('resources.urls')),
+    path('ckeditor/',   include('ckeditor_uploader.urls')),
+    path('correction/', include('correction.urls')),
+    path('logout/',     LogoutView.as_view(next_page='correction:soumettre'), name='logout'),
+    path('chaining/',   include('smart_selects.urls')),
+    path('abonnement/', include('abonnement.urls')),
+    path('paiement/',   include('paiement.urls')),
 
-path(
-    'chaining/',
-    include('smart_selects.urls')),
+    # --- APIs REST ---
 
-path(
-    'abonnement/',
-    include('abonnement.urls')),
-
-path(
-    'paiement/',
-    include('paiement.urls')),
-
-path(
-    'api/',
-    include('correction.api_urls')),
-
+    # 1) Abonnement (doit être prioritaire)
     path(
         'api/abonnement/',
-        include(('abonnement.api_urls', 'abonnement_api'),
-                                  namespace='abonnement_api')),
+        include(('abonnement.api_urls', 'abonnement_api')),
+        name='abonnement_api'
+    ),
 
+    # 2) Paiement (prioritaire aussi)
     path(
         'api/paiement/',
-        include(('paiement.api_urls', 'paiement_api'),
-                                  namespace='paiement_api')),
+        include(('paiement.api_urls', 'paiement_api')),
+        name='paiement_api'
+    ),
 
+    # 3) Tout le reste (correction, listes, profil, etc.)
+    path(
+        'api/',
+        include(('correction.api_urls', 'correction_api'))
+    ),
 ]
+
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
