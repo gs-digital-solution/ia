0c3ce1830b89950d81331ea27323732d84e62409
gs-digital-solution <titus.asaph18@gmail.com>
2025-09-14 17:17:54 +0200

    ok

--- cis/urls.py
+++ cis/urls.py
@@ -1,19 +1,3 @@
-"""
-URL configuration for cis project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
@@ -21,10 +5,9 @@ from django.urls import path, include
 from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-
-    # ... vos routes “resources/”, “ckeditor/”, “correction/”, etc. ...
-    path('resources/', include('resources.urls')),
+    # Interface Web & Admin
+    path('admin/',      admin.site.urls),
+    path('resources/',  include('resources.urls')),
     path('ckeditor/',   include('ckeditor_uploader.urls')),
     path('correction/', include('correction.urls')),
     path('logout/',     LogoutView.as_view(next_page='correction:soumettre'), name='logout'),
@@ -32,27 +15,8 @@ urlpatterns = [
     path('abonnement/', include('abonnement.urls')),
     path('paiement/',   include('paiement.urls')),
 
-    # --- APIs REST ---
-
-    # 1) Abonnement (doit être prioritaire)
-    path(
-        'api/abonnement/',
-        include(('abonnement.api_urls', 'abonnement_api')),
-        name='abonnement_api'
-    ),
-
-    # 2) Paiement (prioritaire aussi)
-    path(
-        'api/paiement/',
-        include(('paiement.api_urls', 'paiement_api')),
-        name='paiement_api'
-    ),
-
-    # 3) Tout le reste (correction, listes, profil, etc.)
-    path(
-        'api/',
-        include(('correction.api_urls', 'correction_api'))
-    ),
+    # Toutes les API REST passent par correction.api_urls
+    path('api/', include(('correction.api_urls', 'correction_api'), namespace='correction_api')),
 ]
 
 if settings.DEBUG:
--- correction/api_urls.py
+++ correction/api_urls.py
@@ -1,4 +1,6 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
 from .api_views import (
     UserRegisterAPIView,
     UserLoginAPIView,
@@ -18,34 +20,40 @@ from .api_views import (
     PartagerCorrigeAPIView,
     DebugExtractionAPIView,
 )
-from resources.api_views import PaysListAPIView, SousSystemeListAPIView
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-app_name = 'correction_api'
+# On importe les routes d’abonnement/paiement/ressources ici
 urlpatterns = [
+    # Authentification & profil
     path('register/',    UserRegisterAPIView.as_view(),      name='api_register'),
     path('login/',       UserLoginAPIView.as_view(),         name='api_login'),
     path('device-migration/', CreateDeviceMigrationRequestAPIView.as_view(), name='api_device_migration'),
-
-    # ← Versions publiques, sans authentification
-    path('pays/',         PaysListAPIView.as_view(),         name='api_pays_list'),
-    path('sous-systeme/', SousSystemeListAPIView.as_view(),  name='api_soussysteme_list'),
-
     path('password-reset/', PasswordResetAPI.as_view(),      name='api_password_reset'),
-    path('profile/',        ProfileAPIView.as_view(),         name='api_profile'),
+    path('profile/',      ProfileAPIView.as_view(),          name='api_profile'),
+    path('token/',        TokenObtainPairView.as_view(),     name='token_obtain_pair'),
+    path('token/refresh/',TokenRefreshView.as_view(),        name='token_refresh'),
 
-    path('token/',          TokenObtainPairView.as_view(),    name='token_obtain_pair'),
-    path('token/refresh/',  TokenRefreshView.as_view(),       name='token_refresh'),
+    # Abonnement
+    path('abonnement/', include(('abonnement.api_urls', 'abonnement_api')), name='abonnement_api'),
 
-    path('soumission/',                     SoumissionExerciceAPIView.as_view(),      name='api_soumission'),
+    # Paiement
+    path('paiement/', include(('paiement.api_urls', 'paiement_api')), name='paiement_api'),
+
+    # Ressources : pays et sous-systèmes
+    path('pays/',         include(('resources.api_urls', 'resources_api')), name='resources_api'),
+    # (resources.api_urls contient les routes 'pays/' et 'sous-systeme/')
+
+    # Soumission d’exercice & statut
+    path('soumission/',                    SoumissionExerciceAPIView.as_view(),      name='api_soumission'),
     path('soumission/<int:soumission_id>/status/', StatutSoumissionAPIView.as_view(), name='api_soumission_status'),
 
+    # Listes protégées
     path('departements/',  DepartementsListAPIView.as_view(),   name='api_departements'),
-    path('classes/',       ClassesListAPIView.as_view(),        name='api_classes'),
-    path('matieres/',      MatieresListAPIView.as_view(),       name='api_matieres'),
-    path('types-exercice/',TypesExerciceListAPIView.as_view(),  name='api_types_exercice'),
-    path('lecons/',        LeconsListAPIView.as_view(),         name='api_lecons'),
+    path('classes/',       ClassesListAPIView.as_view(),       name='api_classes'),
+    path('matieres/',      MatieresListAPIView.as_view(),      name='api_matieres'),
+    path('types-exercice/',TypesExerciceListAPIView.as_view(), name='api_types_exercice'),
+    path('lecons/',        LeconsListAPIView.as_view(),        name='api_lecons'),
 
+    # Corrigé / historique / feedback / partage / debug
     path('soumission/<int:soumission_id>/download/', DownloadCorrigeAPIView.as_view(), name='api_download_corrige'),
     path('historique/',    HistoriqueCorrectionsAPIView.as_view(), name='api_historique'),
     path('feedback/<int:correction_id>/', FeedbackAPIView.as_view(), name='api_feedback'),
