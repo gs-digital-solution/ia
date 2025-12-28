edd4ae9b8fd5070a05d797a17200a5ca7ecc2b59
gs-digital-solution <titus.asaph18@gmail.com>
2025-12-28 16:10:02 +0100

    Retire la dépendance inexistante vers resources.0001_initial

--- correction/migrations/0001_initial.py
+++ correction/migrations/0001_initial.py
@@ -14,7 +14,6 @@ class Migration(migrations.Migration):
 
     dependencies = [
         ('auth', '0012_alter_user_first_name_max_length'),
-        ('resources', '0001_initial'),
     ]
 
     operations = [
