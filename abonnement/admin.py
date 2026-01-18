8aad40eb05ecbd305d357fd99a517c8b0d91bd16
gs-digital-solution <titus.asaph18@gmail.com>
2026-01-18 15:22:06 +0100

    ok

--- abonnement/admin.py
+++ abonnement/admin.py
@@ -1,72 +1,35 @@
 from django.contrib import admin
 from django.db.models import Q
 from .models import SubscriptionType, PromoCode, UserAbonnement, GlobalSubscriptionConfig
-from correction.models import CustomUser  # IMPORT AJOUTÉ
+
+# Utilisez get_user_model au lieu d'importer directement
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 
 class UserAbonnementAdmin(admin.ModelAdmin):
-    """Admin personnalisé pour UserAbonnement avec recherche par numéro WhatsApp"""
+    list_display = ('utilisateur', 'abonnement', 'date_debut', 'statut')
+    list_filter = ('abonnement', 'statut')
 
-    # Champs affichés dans la liste
-    list_display = ('utilisateur', 'get_whatsapp_number', 'abonnement', 'date_debut', 'date_fin', 'exercice_restants',
-                    'statut')
-    list_filter = ('abonnement', 'statut', 'date_debut')
-
-    # Barre de recherche pour trouver un utilisateur
+    # Version SIMPLIFIÉE de search_fields
     search_fields = (
-        'utilisateur__username',  # Nom d'utilisateur
-        'utilisateur__first_name',  # Prénom
-        'utilisateur__last_name',  # Nom de famille
-        'utilisateur__email',  # Email
-        'utilisateur__whatsapp_number',  # Numéro WhatsApp
-        'utilisateur__gmail',  # Gmail
-        'abonnement__nom',  # Nom de l'abonnement
+        'utilisateur__username',
+        'utilisateur__email',
     )
 
-    # Pour une recherche plus précise par numéro WhatsApp
-    def get_search_results(self, request, queryset, search_term):
-        queryset, use_distinct = super().get_search_results(request, queryset, search_term)
+    # Enlevez get_search_results temporairement
+    # Enlevez get_whatsapp_number temporairement
 
-        if search_term:
-            # Recherche aussi par numéro WhatsApp (avec nettoyage)
-            phone_clean = search_term.strip()
-
-            # Nettoyer le numéro pour la recherche
-            # Retirer +237, 237, espaces, etc.
-            if phone_clean.startswith('+237'):
-                phone_clean = phone_clean[4:]  # Retire +237
-            elif phone_clean.startswith('237'):
-                phone_clean = phone_clean[3:]  # Retire 237
-
-            # Retirer tous les caractères non numériques
-            phone_clean = ''.join(filter(str.isdigit, phone_clean))
-
-            if phone_clean:
-                # Recherche le numéro nettoyé
-                queryset |= self.model.objects.filter(
-                    Q(utilisateur__whatsapp_number__icontains=phone_clean)
-                )
-
-        return queryset, use_distinct
-
-    # Méthode pour afficher le numéro WhatsApp dans la liste
-    def get_whatsapp_number(self, obj):
-        return obj.utilisateur.whatsapp_number if obj.utilisateur.whatsapp_number else "-"
-
-    get_whatsapp_number.short_description = "Numéro WhatsApp"
-    get_whatsapp_number.admin_order_field = 'utilisateur__whatsapp_number'
-
-    # Champs à afficher dans le formulaire d'édition
     fieldsets = (
         ('Utilisateur', {
             'fields': ('utilisateur', 'abonnement', 'code_promo_utilise')
         }),
-        ('Détails de l\'abonnement', {
+        ('Détails', {
             'fields': ('date_debut', 'date_fin', 'exercice_restants', 'statut')
         }),
     )
 
-    # Ajout d'un champ de recherche rapide dans le formulaire d'ajout
     raw_id_fields = ('utilisateur',)
 
     # Nombre d'éléments par page
